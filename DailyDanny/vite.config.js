12955facb116af5c8a390be05456eac9eeeab6de
Andrew Valentin <95504743+andrew-valentin@users.noreply.github.com>
2024-04-12 12:57:21 -0400

    Update vite.config.js

--- DailyDanny/vite.config.js
+++ DailyDanny/vite.config.js
@@ -2,6 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	build: {
+		emptyOutDir: true,
+		outDir: 'build'
+	},
 	plugins: [sveltekit()]
 });
 
